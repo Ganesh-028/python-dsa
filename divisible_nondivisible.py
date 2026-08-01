4b8ad683cccd431f403b8b990d7b1323c2dbc0f6
Siva Sai Vishnu Ganesh Yalla <yallaganesh28@gmail.com>
2026-08-01 09:41:11 +0530

    Implement differenceOfSums method in Solution class

---
+++ divisible_nondivisible.py
@@ -0,0 +1,11 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        x =[]
+        y =[]
+        for i in range(1,n+1):
+            if i % m == 0:
+                x.append(i)
+            else:
+                y.append(i)
+        return sum(y)-sum(x)
+        
